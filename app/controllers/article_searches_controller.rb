d08d0a2dc24d5377addb27946e64f29522c2fb92
Brian Buchalter <bal711@gmail.com>
2018-02-08 16:55:38 -0700

    Naive @articles assignment in ArticleSearchesCtrl

--- app/controllers/article_searches_controller.rb
+++ app/controllers/article_searches_controller.rb
@@ -1,5 +1,6 @@
 class ArticleSearchesController < ApplicationController
   def create
+    @articles = Article.all
     render nothing: true
   end
 end
--- spec/controllers/article_searches_controller_spec.rb
+++ spec/controllers/article_searches_controller_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe ArticleSearchesController do
   describe 'POST create' do
-    it 'is successful' do
-      post :create
-      expect(response.status).to eq(200)
+    context 'when an article exists' do
+      let!(:article) do
+        FactoryGirl.create(:article)
+      end
+
+      it 'is successful' do
+        post :create
+        expect(response.status).to eq(200)
+        expect(assigns(:articles)).to eq [article]
+      end
     end
   end
 end
