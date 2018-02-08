dc6f89c4f342e122de5866e82eee646aee8533df
Brian Buchalter <bal711@gmail.com>
2018-02-07 18:39:49 -0700

    Assert ArticleImport creates an Author
    Seems like a baby step, right? Maybe a silly one? Well, that's TDD my
    friends!

--- app/lib/engblog/article_import.rb
+++ app/lib/engblog/article_import.rb
@@ -3,5 +3,14 @@ module Engblog
   # create Articles, HeroImages, and Authors
   # and associate them correctly.
   class ArticleImport
+    # @param file [Pathname] The path to the article file.
+    def initialize(file:)
+    end
+
+    def call
+      Author.create!(
+        name: "Anyone!"
+      )
+    end
   end
 end
--- spec/lib/engblog/article_import_spec.rb
+++ spec/lib/engblog/article_import_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Engblog::ArticleImport do
-  it { is_expected.to be }
+  context "when given a CSV" do
+    subject do
+      described_class.new(
+        file: file_fixture("articles.csv")
+      ).call
+    end
+
+    it "creates an author" do
+      expect { subject }.to change {
+        Author.count
+      }.from(0).to(1)
+    end
+  end
 end
