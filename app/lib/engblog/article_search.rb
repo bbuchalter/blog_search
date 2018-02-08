d5674a0df74c106018b5b9125d53218cd8515d5a
Brian Buchalter <bal711@gmail.com>
2018-02-08 16:17:54 -0700

    Create ArticleSearch

---
+++ app/lib/engblog/article_search.rb
@@ -0,0 +1,6 @@
+module Engblog
+  # Given a query,
+  # return IDs of Articles which contain that query in the body or title.
+  class ArticleSearch
+  end
+end
---
+++ spec/lib/engblog/article_search_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe Engblog::ArticleSearch do
+  it { is_expected.to be }
+end
