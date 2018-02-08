15d59bc97f37c01d105186e158792a7a3a230db4
Brian Buchalter <bal711@gmail.com>
2018-02-08 16:30:19 -0700

    Search titles in ArticleSearch

--- app/lib/engblog/article_search.rb
+++ app/lib/engblog/article_search.rb
@@ -4,10 +4,23 @@ module Engblog
   class ArticleSearch
     # @param query [String] The search term
     def initialize(query:)
+      @query = query
     end
 
     def call
-      [Article.first.id]
+      Article.where(title_match).pluck(:id)
+    end
+
+    private
+
+    attr_reader :query
+
+    def title_match
+      @title_match ||= Article.arel_table[:title].matches(wildcard_query)
+    end
+
+    def wildcard_query
+      @wildcard_query ||= "%#{query}%"
     end
   end
 end
--- spec/lib/engblog/article_search_spec.rb
+++ spec/lib/engblog/article_search_spec.rb
@@ -6,15 +6,19 @@ describe Engblog::ArticleSearch do
   context "when searching 'dox'" do
     let(:query) { 'dox' }
 
-    context "when an article exists" do
-      let!(:article) do
-        FactoryGirl.create(:article, title: title)
-      end
+    context "when many articles exist" do
+      before { 2.times { FactoryGirl.create(:article) } }
 
-      context "with the title 'Dox is the next big thing'" do
-        let(:title) { 'Dox is the next big thing' }
-        it "returns the ID of that article" do
-          expect(subject).to include(article.id)
+      context "when an article exists" do
+        let!(:article) do
+          FactoryGirl.create(:article, title: title)
+        end
+
+        context "with the title 'Dox is the next big thing'" do
+          let(:title) { 'Dox is the next big thing' }
+          it "returns the ID of that article" do
+            expect(subject).to include(article.id)
+          end
         end
       end
     end
