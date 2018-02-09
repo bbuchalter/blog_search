0596aea58b8b0d27890fe91b6c01f444e091a47b
Brian Buchalter <bal711@gmail.com>
2018-02-09 02:04:59 -0700

    Create CalcWordScores

---
+++ app/lib/engblog/calc_word_scores.rb
@@ -0,0 +1,7 @@
+module Engblog
+  # Given an array of words and weight,
+  # Calculate a "score" for each word such that
+  # score = weight * count(word)
+  class CalcWordScores
+  end
+end
---
+++ spec/lib/engblog/calc_word_scores_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require_relative '../../../app/lib/engblog/calc_word_scores'
+
+RSpec.describe Engblog::CalcWordScores do
+  it { is_expected.to be }
+end
