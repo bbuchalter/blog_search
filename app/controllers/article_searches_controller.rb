79e5a2950cc789c6fa63e192b29ba7856844af7a
Brian Buchalter <bal711@gmail.com>
2018-02-08 17:03:50 -0700

    ArticleSearchCtrl shows only published matches

--- app/controllers/article_searches_controller.rb
+++ app/controllers/article_searches_controller.rb
@@ -1,6 +1,12 @@
 class ArticleSearchesController < ApplicationController
   def create
-    @articles = Article.all
+    @articles = Article.published
+    if params[:query]
+      @articles = @articles.where(
+        id: Engblog::ArticleSearch.new(query: params[:query]).call
+      )
+    end
+
     render nothing: true
   end
 end
--- spec/controllers/article_searches_controller_spec.rb
+++ spec/controllers/article_searches_controller_spec.rb
@@ -7,10 +7,33 @@ describe ArticleSearchesController do
         FactoryGirl.create(:article)
       end
 
-      it 'is successful' do
+      it 'is assigns the articles successfully' do
         post :create
         expect(response.status).to eq(200)
         expect(assigns(:articles)).to eq [article]
+      end
+
+      context 'when query is provided' do
+        let(:query) { 'Dox' }
+        context 'when a matching article exists' do
+          let!(:dox_article) { FactoryGirl.create(:article, title: 'Dox') }
+
+          it 'constrains the articles assigned to only matches' do
+            post :create, params: { query: 'Dox' }
+            expect(response.status).to eq(200)
+            expect(assigns(:articles)).to eq [dox_article]
+          end
+        end
+      end
+
+      context 'when an unpublished article exists' do
+        before { FactoryGirl.create(:article, :unpublished) }
+
+        it 'is ignored' do
+          post :create
+          expect(response.status).to eq(200)
+          expect(assigns(:articles)).to eq [article]
+        end
       end
     end
   end
