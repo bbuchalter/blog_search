6ae0e422570d4f2daf7c619153188f9147913e7c
Brian Buchalter <bal711@gmail.com>
2018-02-09 02:14:24 -0700

    Implement CalcWordScores

--- app/lib/engblog/calc_word_scores.rb
+++ app/lib/engblog/calc_word_scores.rb
@@ -3,5 +3,20 @@ module Engblog
   # Calculate a "score" for each word such that
   # score = weight * count(word)
   class CalcWordScores
+    def initialize(words:, weight:)
+      @words = words
+      @weight = weight
+    end
+
+    def call
+      words.reduce(Hash.new(0)) do |word_scores, word|
+        word_scores[word] += weight
+        word_scores
+      end
+    end
+
+    private
+
+    attr_reader :words, :weight
   end
 end
--- spec/lib/engblog/calc_word_scores_spec.rb
+++ spec/lib/engblog/calc_word_scores_spec.rb
@@ -2,5 +2,26 @@ require 'spec_helper'
 require_relative '../../../app/lib/engblog/calc_word_scores'
 
 RSpec.describe Engblog::CalcWordScores do
-  it { is_expected.to be }
+  subject { described_class.new(words: words, weight: weight).call }
+
+  context "when words are %w(dox loves doctors doctors love dox why not)" do
+    let(:words) { %w(dox loves doctors doctors love dox why not) }
+
+    context "and the weight is 2" do
+      let(:weight) { 2 }
+
+      it "scores each word by multiplying its number of occurances by the weight" do
+        expect(subject).to eq(
+          {
+            "dox" => 4,
+            "loves" => 2,
+            "doctors" => 4,
+            "love" => 2,
+            "why" => 2,
+            "not" => 2
+          }
+        )
+      end
+    end
+  end
 end
