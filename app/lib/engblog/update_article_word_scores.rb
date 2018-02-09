914174a1bb942f93770b19dfcb0bb2b730c35926
Brian Buchalter <bal711@gmail.com>
2018-02-09 04:08:29 -0700

    Implement UpdateArticleWordScores

--- app/lib/engblog/update_article_word_scores.rb
+++ app/lib/engblog/update_article_word_scores.rb
@@ -9,10 +9,47 @@ module Engblog
 
     def call
       WordScore.where(article_id: article.id).destroy_all
+
+      article_word_scores.each do |word, score|
+        WordScore.create!(
+          article: article,
+          word: word,
+          score: score
+        )
+      end
     end
 
     private
 
     attr_reader :article
+
+    def article_word_scores
+      AddWordScores.new(
+        title_word_scores,
+        body_word_scores
+      ).call
+    end
+
+    def title_word_scores
+      CalcWordScores.new(
+        words: title_search_words,
+        weight: 10
+      ).call
+    end
+
+    def title_search_words
+      TextToSearchWords.new(text: article.title).call
+    end
+
+    def body_word_scores
+      CalcWordScores.new(
+        words: body_search_words,
+        weight: 1
+      ).call
+    end
+
+    def body_search_words
+      TextToSearchWords.new(text: article.body).call
+    end
   end
 end
--- spec/lib/engblog/update_article_word_scores_spec.rb
+++ spec/lib/engblog/update_article_word_scores_spec.rb
@@ -4,15 +4,39 @@ RSpec.describe Engblog::UpdateArticleWordScores do
   let(:article) { FactoryGirl.create(:article) }
   subject { described_class.new(article: article).call }
 
-  context "when an article already has a word score" do
-    let!(:existing_word_score) do
-      FactoryGirl.create(:word_score, article: article)
+  context "when an article has a title of 'doctors loves dox'" do
+    let(:article) do
+      FactoryGirl.create(:article, title: title, body: body)
     end
+    let(:title) { 'doctors loves dox' }
 
-    it "is deleted" do
-      expect { subject }.to change {
-        WordScore.count
-      }.from(1).to(0)
+    context "with a body of 'dox loves rails'" do
+      let(:body) { 'dox loves rails' }
+
+      it "creates a word score for each word" do
+        expect { subject }.to change {
+          WordScore.order(:word).pluck(:word, :score)
+        }.from([]).to(
+          [
+            ["doctors", 10],
+            ["dox", 11],
+            ["loves", 11],
+            ["rails", 1],
+          ]
+        )
+      end
+
+      context "when the article already had some word scores" do
+        before do
+          FactoryGirl.create(:word_score, article: article, word: 'deleted')
+        end
+
+        it "deletes the old scores before creating new ones" do
+          expect { subject }.to change {
+            WordScore.order(:word).pluck(:word)
+          }.from(['deleted']).to(%w(doctors dox loves rails))
+        end
+      end
     end
   end
 end
