6bc8c914dc266916315666a66b2e9d0dffbf0be6
Brian Buchalter <bal711@gmail.com>
2018-02-09 03:48:31 -0700

    Create AddWordScores
    We need to combine the title word scores with the body word scores into
    a single word score for the article.

---
+++ app/lib/engblog/add_word_scores.rb
@@ -0,0 +1,7 @@
+module Engblog
+  # Given two or more word score data structures,
+  # "add" them together such that scores accumulate
+  # and the final structure has all words.
+  class AddWordScores
+  end
+end
---
+++ spec/lib/engblog/add_word_scores_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require_relative '../../../app/lib/engblog/add_word_scores'
+
+RSpec.describe Engblog::AddWordScores do
+  it { is_expected.to be }
+end
