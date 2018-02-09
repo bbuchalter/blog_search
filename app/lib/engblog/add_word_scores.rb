3e53580c38e046ce6fd390695117539cccc0c200
Brian Buchalter <bal711@gmail.com>
2018-02-09 03:56:58 -0700

    Implement AddWordScores

--- app/lib/engblog/add_word_scores.rb
+++ app/lib/engblog/add_word_scores.rb
@@ -3,5 +3,23 @@ module Engblog
   # "add" them together such that scores accumulate
   # and the final structure has all words.
   class AddWordScores
+    def initialize(*word_scores)
+      @word_scores = word_scores
+      @added_scores = Hash.new(0)
+    end
+
+    def call
+      word_scores.each do |word_score|
+        word_score.each do |word, score|
+          added_scores[word] += score
+        end
+      end
+      added_scores
+    end
+
+    private
+
+    attr_reader :word_scores
+    attr_accessor :added_scores
   end
 end
--- spec/lib/engblog/add_word_scores_spec.rb
+++ spec/lib/engblog/add_word_scores_spec.rb
@@ -2,5 +2,25 @@ require 'spec_helper'
 require_relative '../../../app/lib/engblog/add_word_scores'
 
 RSpec.describe Engblog::AddWordScores do
-  it { is_expected.to be }
+  context "when given a word score of { 'dox' => 1, 'first' => 10 }" do
+    let(:first) { { 'dox' => 1, 'first' => 10} }
+
+    context "when also given a word score of { 'dox' => 2, 'second' => 20 }" do
+      let(:second) { { 'dox' => 2, 'second' => 20 } }
+
+      subject do
+        described_class.new(first, second).call
+      end
+
+      it "adds the scores together" do
+        expect(subject).to eq(
+          {
+            'dox' => 3,
+            'first' => 10,
+            'second' => 20
+          }
+        )
+      end
+    end
+  end
 end
