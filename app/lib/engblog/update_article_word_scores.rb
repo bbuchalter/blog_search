2a1cdcbc812abdc1d4c84ba2a2ad06c14df27243
Brian Buchalter <bal711@gmail.com>
2018-02-09 02:39:52 -0700

    Create UpdateArticleWordScores

---
+++ app/lib/engblog/update_article_word_scores.rb
@@ -0,0 +1,7 @@
+module Engblog
+  # Given an Article,
+  # remove any existing word scores for the article,
+  # calculate new word scores and record them.
+  class UpdateArticleWordScores
+  end
+end
