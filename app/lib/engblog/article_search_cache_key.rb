d96b1d76f099b134077ee68a95f98738c06285cc
Brian Buchalter <bal711@gmail.com>
2018-02-17 09:20:42 -0700

    Implement ArticleSearchCacheKey
    We need to be able to uniquely identify an ArticleSearch query and the results
    we expect it to return in order to avoid looking up results repeatedly.

---
+++ app/lib/engblog/article_search_cache_key.rb
@@ -0,0 +1,42 @@
+module Engblog
+  class ArticleSearchCacheKey
+    def initialize(page:, query:)
+      @page = page
+      @query = query
+    end
+
+    def call
+      [
+        search_words.sort,
+        page,
+        count_of_word_scores_for_search_words, # OPTIMIZE: 3 queries should be 1
+        max_id_of_word_scores_for_search_words,
+        max_updated_at_of_word_scores_for_search_words.to_i # TODO: support microsecond precision
+      ].flatten.join('-')
+    end
+
+    private
+
+    attr_reader :page, :query
+
+    def search_words
+      @search_words ||= TextToSearchWords.new(text: query).call
+    end
+
+    def count_of_word_scores_for_search_words
+      word_scores_for_search_words.count
+    end
+
+    def max_id_of_word_scores_for_search_words
+      word_scores_for_search_words.maximum(:id)
+    end
+
+    def max_updated_at_of_word_scores_for_search_words
+      word_scores_for_search_words.maximum(:updated_at)
+    end
+
+    def word_scores_for_search_words
+      @word_scores_for_search_words ||= WordScore.where(word: search_words)
+    end
+  end
+end
---
+++ spec/lib/engblog/article_search_cache_key_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+RSpec.describe Engblog::ArticleSearchCacheKey do
+  subject do
+    described_class.new(
+      page: page,
+      query: query
+    ).call
+  end
+
+  context 'when requesting the first page of results' do
+    let(:page) { 1 }
+
+    context "when query is 'search cache'" do
+      let(:query) { 'search cache' }
+
+      context "when two relevant word scores exist" do
+        before do
+          FactoryGirl.create :word_score, word: 'search'
+          FactoryGirl.create :word_score, word: 'cache'
+        end
+
+        context "when a relevant record is mutated" do
+          it "changes the cache key" do
+            expect { sleep 1; WordScore.find_by(word: 'search').update!(score: 32) }.to change {
+              described_class.new(
+                page: page,
+                query: query
+              ).call
+            }
+          end
+        end
+
+        context "when a relevant record is added" do
+          it "changes the cache key" do
+            expect { FactoryGirl.create :word_score, word: 'cache' }.to change {
+              described_class.new(
+                page: page,
+                query: query
+              ).call
+            }
+          end
+        end
+
+        context "when a relevant record is removed" do
+          it "changes the cache key" do
+            expect { WordScore.where(word: 'search').destroy_all }.to change {
+              described_class.new(
+                page: page,
+                query: query
+              ).call
+            }
+          end
+        end
+
+        context "when the query changes" do
+          before { @query = query }
+          it "changes the cache key" do
+            expect { @query = 'something new' }.to change {
+              described_class.new(
+                page: page,
+                query: @query
+              ).call
+            }
+          end
+        end
+
+        context "when a new page of results are requested" do
+          before { @page = page }
+          it "changes the cache key" do
+            expect { @page = 2 }.to change {
+              described_class.new(
+                page: @page,
+                query: query
+              ).call
+            }
+          end
+        end
+      end
+    end
+  end
+end
