2f4ed9ea68c47bf70d863a65cbd89420e464edee
Brian Buchalter <bal711@gmail.com>
2018-02-19 11:51:56 -0700

    Reorg CachedArticleSearchSpec

--- spec/lib/engblog/cached_article_search_spec.rb
+++ spec/lib/engblog/cached_article_search_spec.rb
@@ -1,38 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Engblog::CachedArticleSearch do
-  describe "#results" do
-    subject do
-      described_class.new(
-        query: query,
-        page: page,
-        per_page: per_page
-      ).results
-    end
+  context "given a query 'dox'" do
+    let(:query) { 'dox' }
 
-    context "given a query 'dox'" do
-      let(:query) { 'dox' }
+    context "given an article with the title 'dox'" do
+      let!(:title_match_article) { FactoryGirl.create :article, title: 'dox' }
 
-      context "given an article with the title 'dox'" do
-        let!(:title_match_article) { FactoryGirl.create :article, title: 'dox' }
+      context "given an article with the body 'dox'" do
+        let!(:body_match_article) { FactoryGirl.create :article, body: 'dox' }
 
-        context "given an article with the body 'dox'" do
-          let!(:body_match_article) { FactoryGirl.create :article, body: 'dox' }
+        context "given those articles have been WordScored" do
 
-          context "given those articles have been WordScored" do
+          context "given we ask for the first page of results" do
+            let(:page) { 1 }
 
-            context "given we ask for the first page of results" do
-              let(:page) { 1 }
+            context "given we ask for one article per page" do
+              let(:per_page) { 1 }
 
-              context "given we ask for one article per page" do
-                let(:per_page) { 1 }
-
-                context "given the articles word scores are calculated" do
-                  before do
-                    Article.all.each do |article|
-                      Engblog::UpdateArticleWordScores.new(article: article).call
-                    end
+              context "given the articles word scores are calculated" do
+                before do
+                  Article.all.each do |article|
+                    Engblog::UpdateArticleWordScores.new(article: article).call
                   end
+                end
+
+                describe "#results" do
+                  subject do
+                    described_class.new(
+                      query: query,
+                      page: page,
+                      per_page: per_page
+                    ).results
+                  end
+
 
                   it "returns the articles for that page" do
                     expect(subject).to eq([title_match_article])
